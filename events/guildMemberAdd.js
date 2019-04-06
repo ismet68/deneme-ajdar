17d3490d8589652325a246ab69aa95e93121e40c
ismet68 <49348476+ismet68@users.noreply.github.com>
2019-04-06 21:57:33 +0300

    Add files via upload

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,5 @@
+module.exports = member => {
+    let username = member.user.username;
+    member.sendMessage('Selamlar, **' + username + '** Ekibe Sunucuma Hoşgeldin :kissing_heart: #kurallar a bakmayı unutma,lazım olacağından eminim. :thinking:');
+
+};
